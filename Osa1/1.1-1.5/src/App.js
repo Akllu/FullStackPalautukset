5110ac74ef2e8c54dc7e61a2b4a0de70e904eaab
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2021-05-07 18:24:43 +0300

    Tehtävät tehty 1.11 asti

--- Osa1/1.1-1.5/src/App.js
+++ Osa1/1.1-1.5/src/App.js
@@ -1,33 +1,5 @@
 import React from 'react'
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course}/>
-      <Total course={course}/>
-    </div>
-  )
-}
-
 const Header = (props) => {
   return (
     <h1>{props.course.name}</h1>
@@ -58,6 +30,34 @@ const Part = (props) => {
       <p>
         {props.pt}, exercises: {props.exrc}
       </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
---
+++ Osa1/1.12-1.14/src/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+  )
+
+const StatisticLine = ({ value, text}) => (
+  <div>
+    <div>{text} {value} </div>
+  </div>
+)
+
+const Statistics = (props) => {
+  if (props.clicks.allClicks === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback has been given yet!</p>   
+      </div>
+    )
+  }
+  const avrg = props.clicks.average / props.clicks.allClicks
+  const posPercentage = props.clicks.good / props.clicks.allClicks * 100
+  return (    
+    <table>
+      <tbody>     
+        <tr>
+          <td><StatisticLine value={props.clicks.good} text={"Good feedback:"} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine value={props.clicks.neutral} text={"Neutral feedback:"} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine value={props.clicks.bad} text={"Bad feedback:"} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine value={props.clicks.allClicks} text={"Total feedback:"} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine value={avrg} text={"Average feedback:"} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine value={posPercentage+"%"} text={"Positive percentage of feedback:"} /></td>
+        </tr>
+      </tbody>
+    </table>     
+  )
+}
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0, allClicks: 0, average: 0
+  })
+
+  const handleGoodClick = () => {
+    setClicks({
+      ...clicks,
+      good: clicks.good + 1,
+      allClicks: clicks.allClicks + 1,
+      average: clicks.average + 1
+    })
+  }
+
+  const handleNeutralClick = () => {
+    setClicks({
+      ...clicks,
+      neutral: clicks.neutral + 1,
+      allClicks: clicks.allClicks + 1,
+      average: clicks.average + 0
+    })
+  }
+
+  const handleBadClick = () => {
+    setClicks({
+      ...clicks,
+      bad: clicks.bad + 1,
+      allClicks: clicks.allClicks + 1,
+      average: clicks.average - 1
+    })
+  }
+ 
+  return (
+    <div>
+      <h1> Give feedback: </h1>
+      <Button handleClick={handleGoodClick} text="Good :)" />
+      <Button handleClick={handleNeutralClick} text="Neutral :I" />
+      <Button handleClick={handleBadClick} text="Bad :(" />
+      <Statistics clicks={clicks} />
+    </div>
+  )
+}
+
+export default App
