96f48bb964c5455daf0cc8a240189adbdbc3034a
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2021-05-08 12:33:15 +0300

    Tehtävät tehty 1.14 asti

--- Osa1/1.12-1.14/src/App.js
+++ Osa1/1.12-1.14/src/App.js
@@ -1,93 +1,59 @@
 import React, { useState } from 'react'
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+     {props.text}
+    </button>
   )
+}
 
-const StatisticLine = ({ value, text}) => (
-  <div>
-    <div>{text} {value} </div>
-  </div>
-)
-
-const Statistics = (props) => {
-  if (props.clicks.allClicks === 0) {
-    return (
-      <div>
-        <h1>Statistics</h1>
-        <p>No feedback has been given yet!</p>   
-      </div>
-    )
-  }
-  const avrg = props.clicks.average / props.clicks.allClicks
-  const posPercentage = props.clicks.good / props.clicks.allClicks * 100
-  return (    
-    <table>
-      <tbody>     
-        <tr>
-          <td><StatisticLine value={props.clicks.good} text={"Good feedback:"} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine value={props.clicks.neutral} text={"Neutral feedback:"} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine value={props.clicks.bad} text={"Bad feedback:"} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine value={props.clicks.allClicks} text={"Total feedback:"} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine value={avrg} text={"Average feedback:"} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine value={posPercentage+"%"} text={"Positive percentage of feedback:"} /></td>
-        </tr>
-      </tbody>
-    </table>     
+const Display = (props) => {
+  return (
+    <div>
+      <h1> Anecdote of the day </h1> 
+      {props.anecdote} 
+      <p> This anecdote has {props.votes} votes </p>
+      <h2> Anecdote with most votes</h2>
+      {props.mostVoted}
+    </div>
   )
 }
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    good: 0, neutral: 0, bad: 0, allClicks: 0, average: 0
-  })
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'The goal of Computer Science is to build something that will last at least until we have finished building it.',
+    'Why do we never have time to do it right, but always have time to do it over?',
+    'A good way to stay flexible is to write less code.',
+    'Always code as if the person who ends up maintaining your code will be a violent psychopath who knows where you live.'
+  ]
+   
+  const [selected, nextClicked] = useState(0)
+  const [array, voteClicked] = useState(new Uint8Array(10))
 
-  const handleGoodClick = () => {
-    setClicks({
-      ...clicks,
-      good: clicks.good + 1,
-      allClicks: clicks.allClicks + 1,
-      average: clicks.average + 1
-    })
-  }
+  const mostVotes = Math.max(...array)
+  const maxIndex = array.indexOf(mostVotes)
 
-  const handleNeutralClick = () => {
-    setClicks({
-      ...clicks,
-      neutral: clicks.neutral + 1,
-      allClicks: clicks.allClicks + 1,
-      average: clicks.average + 0
-    })
-  }
+  const random = () => Math.floor(Math.random() * 10)
+    
+  const selectNext = () => nextClicked(random)        
 
-  const handleBadClick = () => {
-    setClicks({
-      ...clicks,
-      bad: clicks.bad + 1,
-      allClicks: clicks.allClicks + 1,
-      average: clicks.average - 1
-    })
+  const selectVote = () => {
+    const copy = [...array]
+    copy[selected] += 1
+    voteClicked(copy)
   }
- 
   return (
     <div>
-      <h1> Give feedback: </h1>
-      <Button handleClick={handleGoodClick} text="Good :)" />
-      <Button handleClick={handleNeutralClick} text="Neutral :I" />
-      <Button handleClick={handleBadClick} text="Bad :(" />
-      <Statistics clicks={clicks} />
+      <Display anecdote={anecdotes[selected]} votes={array[selected]} mostVoted={anecdotes[maxIndex]} />
+      <Button handleClick={selectNext} text={"Next anecdote"} />   
+      <Button handleClick={selectVote} text={"Vote"} />
     </div>
   )
 }
