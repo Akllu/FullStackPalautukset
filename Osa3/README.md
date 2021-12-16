544e63e4c501000184eef13bf0cc2b89dc88068f
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2021-12-16 18:32:34 +0200

    Osa 3 valmis

---
+++ Osa3/README.md
@@ -0,0 +1,4 @@
+# FullStack-Osa3
+
+### Linkki sovellukseen:
+https://whispering-scrubland-78139.herokuapp.com/
---
+++ Osa3/index.js
@@ -0,0 +1,111 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+const Person = require('./models/person')
+
+morgan.token('post', (req) => {
+  if (req.method === 'POST') return JSON.stringify(req.body)
+})
+
+morgan.format('custom', ':method :url :status :res[content-length] - :response-time ms :post')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+app.use(morgan('custom'))
+
+app.get('/info', (req, res) => {
+  Person.find({}).then(persons => {
+    res.send(`<div>Phonebook has info for ${persons.length} people</div><p>${Date()}</p>`)
+  })
+})
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person.toJSON())
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'Something is missing!'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(savedPerson => res.json(savedPerson.toJSON()))
+    .then(savedAndFormattedPerson => {
+      res.json(savedAndFormattedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then (updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (res) => {
+  res.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'Malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
